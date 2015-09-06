2544639ee857ba16889ede911ee6191fd810fee2
yyccbbz <juancan@163.com>
2015-09-06 09:26:25 +0800

    修改配置spring配置文件

--- ssm/src/main/java/org/yyccbbz/ssm/controller/ItemController.java
+++ ssm/src/main/java/org/yyccbbz/ssm/controller/ItemController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yyccbbz.ssm.pojo.Item;
-import org.yyccbbz.ssm.pojo.ItemQueryVO;
 import org.yyccbbz.ssm.service.ItemService;
 
 @Controller
